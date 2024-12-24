4482cedf038a36310e2893c782c6baa0592c73e2
Balazs Nemeth <bnemeth@redhat.com>
2024-12-24 19:18:54 +0100

    wip

--- ipu-plugin/build.sh
+++ ipu-plugin/build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# cp -r ../e2e/artefacts/fxp-net_linux-networking .
-# mkdir -p ./bin && cp -r ../e2e/artefacts/bin/* ./bin/
-# podman build -t intel-vsp -f images/Dockerfile . --no-cache
-# podman image tag intel-vsp intel-vsp
 cd /root/ipu-opi-plugins/ipu-plugin/
 
 cp -r ../e2e/artefacts/fxp-net_linux-networking .
