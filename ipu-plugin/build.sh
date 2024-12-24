55996317da79e9e7b40fe6bcf4a4c72afd69a4f1
Balazs Nemeth <bnemeth@redhat.com>
2024-12-24 13:54:38 +0100

    wip

--- ipu-plugin/build.sh
+++ ipu-plugin/build.sh
@@ -10,4 +10,4 @@ mkdir -p ./bin && cp -r ../e2e/artefacts/bin/* ./bin/
 
 buildah manifest rm intel-vsp-manifest || true
 buildah manifest create intel-vsp-manifest
-buildah build --authfile /root/config.json --manifest intel-vsp-manifest --platform linux/amd64,linux/arm64 -f Dockerfile -t intel-vsp
+buildah build --authfile /root/config.json --manifest intel-vsp-manifest --platform linux/amd64,linux/arm64 -f Dockerfile -t intel-ipuplugin:latest
