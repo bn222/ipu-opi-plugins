d23c072de49a642d620d7276802b2fa1aca38d18
Balazs Nemeth <bnemeth@redhat.com>
2024-12-24 13:37:45 +0100

    wip

---
+++ ipu-plugin/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# cp -r ../e2e/artefacts/fxp-net_linux-networking .
+# mkdir -p ./bin && cp -r ../e2e/artefacts/bin/* ./bin/
+# podman build -t intel-vsp -f images/Dockerfile . --no-cache
+# podman image tag intel-vsp intel-vsp
+
+cp -r ../e2e/artefacts/fxp-net_linux-networking .
+mkdir -p ./bin && cp -r ../e2e/artefacts/bin/* ./bin/
+
+buildah manifest rm intel-vsp-manifest || true
+buildah manifest create intel-vsp-manifest
+buildah build --authfile /root/config.json --manifest intel-vsp-manifest --platform linux/amd64,linux/arm64 -f Dockerfile -t intel-vsp
